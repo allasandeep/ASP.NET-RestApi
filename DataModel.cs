ecafccd7b3f51cae05049957de39e6d3aba89bc4
Alla Sandeep <sandeepalla96@gmail.com>
2018-03-21 16:34:41 -0500

    Add files via upload

---
+++ DataModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RestApi.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public int Age { get; set; }
+        public String Location { get; set; }
+    }
+
+    public class datainstance: DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+    }
+}
